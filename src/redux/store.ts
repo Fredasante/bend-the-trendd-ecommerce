7cb8856b468c17254e561f8a9a14bfc4b115e21a
Alfred Apenteng <alfredapenteng6996@gmail.com>
2025-10-12 20:11:14 +0000

    feat(redux): add redux-persist to store for cart and wishlist state persistence

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+"use client";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import quickViewReducer from "./features/quickView-slice";
 import cartReducer from "./features/cart-slice";
@@ -7,16 +19,40 @@ import productDetailsReducer from "./features/product-details";
 
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
-export const store = configureStore({
-  reducer: {
-    quickViewReducer,
-    cartReducer,
-    wishlistReducer,
-    productDetailsReducer,
-  },
+// Combine all slices
+const rootReducer = combineReducers({
+  quickViewReducer,
+  cartReducer,
+  wishlistReducer,
+  productDetailsReducer,
 });
 
+// Configure persist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cartReducer", "wishlistReducer"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// Persistor
+export const persistor = persistStore(store);
+
+// Types
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+// Typed selector
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
