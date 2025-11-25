bb1848b7156517d568a59733250a23ab1054f7a9
Alfred Apenteng <alfredapenteng6996@gmail.com>
2025-11-25 01:36:00 +0000

    sanity product schema update

--- src/sanity/schemas/product.ts
+++ src/sanity/schemas/product.ts
@@ -49,7 +49,6 @@ export const product = defineType({
           { title: "Bags", value: "bags" },
           { title: "Shoes", value: "shoes" },
           { title: "Sneakers", value: "sneakers" },
-          { title: "Gadgets", value: "gadgets" },
         ],
         layout: "dropdown",
       },
