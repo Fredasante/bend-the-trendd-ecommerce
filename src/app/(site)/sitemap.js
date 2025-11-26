5d78c3f473ae2be95aefad1089c739dcc632ae27
Alfred Apenteng <alfredapenteng6996@gmail.com>
2025-11-26 23:07:21 +0000

    fix: correct import path for sanity client in sitemap.js

--- src/app/(site)/sitemap.js
+++ src/app/(site)/sitemap.js
@@ -1,5 +1,5 @@
 import { groq } from "next-sanity";
-import { client } from "@/sanity/lib/client";
+import { client } from "@/sanity/client";
 
 export default async function sitemap() {
   const baseUrl = "https://www.bendthetrendd.com";
