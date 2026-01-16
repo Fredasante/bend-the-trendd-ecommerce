571140fdb8438a3eb2bd77ccbb270bc75993938b
Alfred Apenteng <alfredapenteng6996@gmail.com>
2026-01-16 17:14:42 +0000

    feat: add Google Analytics (GA4) tracking to app layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,6 +3,7 @@ import "../css/style.css";
 import type { Metadata } from "next";
 import ClientLayout from "./ClientLayout";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Bend the Trendd | Fashion & Lifestyle Ecommerce",
@@ -21,6 +22,24 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
+        <head>
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-RZV1DN54HM"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RZV1DN54HM', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </head>
+
         <body>
           <ClientLayout>{children}</ClientLayout>
         </body>
